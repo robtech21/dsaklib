f6c82bd9cc09ff09b83fcf328e5b0b53b2b045be
Robert Furr <77758464+robtech21@users.noreply.github.com>
2021-05-20 08:50:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 setuptools.setup()
 
 #commenting all of this out for now
-#setup(
-#    name='dsaklib',
-#    version='0.0.1',
-#    description='Libraries for Digital Swiss Army Knife',
-#    url='git@github.com:robtech21/dsaklib.git',
-#    author='Robert Furr',
-#    author_email='robert@megley.com',
-#    license='MIT License',
-#    packages=['dsaklib'],
-#    zip_safe=False
-#)
+setup(
+    name='dsaklib',
+    version='0.0.1',
+    description='Libraries for Digital Swiss Army Knife',
+    url='https://github.com/robtech21/dsaklib',
+    author='Robert Furr',
+    author_email='robert@megley.com',
+    license='MIT License',
+    packages=['dsaklib'],
+    zip_safe=False
+)
